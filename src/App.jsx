01b219cd67a7edb962b8896ba554da17ac462416
mrkrca <aleksa.djuric.dj@gmail.com>
2025-03-19 12:25:17 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ProjectSidebar from "./components/ProjectSideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 
 function App() {
   return (
     <>
-     
-      <ProjectSidebar />
-
-     
+      <div className="flex h-screen">
+        <ProjectSidebar />
+        <div className="flex flex-grow items-center justify-center">
+          <NoProjectSelected />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoProjectSelected() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-zinc-900 font-semibold text-4xl">No project selected</h1>
+      <p className="text-xl">Please select a project or get started with the new one</p>
+      <button className="flex bg-gray-600 w-fit p-3 rounded-xl hover:bg-gray-700 text-gray-300 font-semibold text-xl"> Create new project</button>
+    </div>
+  );
+}
+
+export default NoProjectSelected;
